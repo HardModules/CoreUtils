5575d3ac2ad6a0225a426f7da49953ec3d77beeb
Maxim G. <CriDos@users.noreply.github.com>
2024-03-25 01:49:23 +0300

    chore: update default log filename and bump version
    - Updated the default log filename from 'log_.txt' to 'app_.log' in LoggerConfig.cs
    - Bumped package version from 2.2.3 to 2.2.4 in CoreUtils.csproj

--- CoreUtils/Logging/LoggerConfig.cs
+++ CoreUtils/Logging/LoggerConfig.cs
@@ -22,7 +22,7 @@ public class LoggerConfig
     /// <summary>
     /// Gets or sets the log file name.
     /// </summary>
-    public string FileName { get; set; } = "log_.txt";
+    public string FileName { get; set; } = "app_.log";
 
     /// <summary>
     /// Gets or sets the output template for the log messages.
